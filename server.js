3a2c15acc1ff39c19797f65cc5a6a85a7b762f05
Jens de Rond <jensderond@gmail.com>
2017-12-14 15:23:24 +0100

    Prepare for heroku

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // run the app by serving static files
 
 app.use(express.static(__dirname + '/dist'));
+
+app.use(express.static(path.resolve(__dirname, "dist")));
+
+
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 // Start the app by listening on the default
 // Heroku port
